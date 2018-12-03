074ba91e0a0d33598038f228d93a628c98c7eacd
Ammar Ali <Ammar288@users.noreply.github.com>
2018-12-04 00:08:31 +0500

    Add files via upload

---
+++ Code/BonusAssignment/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BonusAssignmentWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Code/BonusAssignment/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BonusAssignmentWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
